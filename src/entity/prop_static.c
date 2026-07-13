c8e0f4bd4036d2e64de4afa7ca62ec40ea703318
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-07-13 12:55:53 +0900

    prop_staticエンティティを作成

---
+++ src/entity/prop_static.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2026, tas0dev.
+ * This software is provided under the zlib License.
+ *
+ * Created by tas0dev
+ *
+ */
+
+#include "entity/prop_static.h"
+#include <stdlib.h>
+
+prop_static_t *prop_static_create(const entity_id_t id,
+				  const mesh_t *mesh,
+				  const material_t *material) {
+	prop_static_t *prop;
+
+	if (mesh == NULL || material == NULL) { return NULL; }
+
+	prop = calloc(1, sizeof(*prop));
+	if (prop == NULL) { return NULL; }
+
+	prop->entity = entity_create(id, "prop_static");
+	prop->mesh = mesh;
+	prop->material = material;
+	prop->casts_shadow = true;
+
+	return prop;
+}
+
+void prop_static_destroy(prop_static_t *prop) { free(prop); }
+
+entity_t *prop_static_get_entity(prop_static_t *prop) {
+	if (prop == NULL) { return NULL; }
+
+	return &prop->entity;
+}
+
+const entity_t *prop_static_get_const_entity(const prop_static_t *prop) {
+	if (prop == NULL) { return NULL; }
+
+	return &prop->entity;
+}
---
+++ src/entity/prop_static.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2026, tas0dev.
+ * This software is provided under the zlib License.
+ *
+ * Created by tas0dev
+ *
+ */
+
+#ifndef VOLUME_ENTITY_PROP_STATIC_H
+#define VOLUME_ENTITY_PROP_STATIC_H
+
+#include "entity/entity.h"
+#include "renderer/material.h"
+#include "renderer/mesh.h"
+
+typedef struct prop_static {
+	entity_t entity;
+	const mesh_t *mesh;
+	const material_t *material;
+	bool casts_shadow;
+} prop_static_t;
+
+prop_static_t *prop_static_create(entity_id_t id,
+				  const mesh_t *mesh,
+				  const material_t *material);
+void prop_static_destroy(prop_static_t *prop);
+entity_t *prop_static_get_entity(prop_static_t *prop);
+const entity_t *prop_static_get_const_entity(const prop_static_t *prop);
+
+#endif
